ea0585ddaa41021a2ddf8a760b0fc865220f6fc3
giangthao <giangvtt@tasc.vn>
2024-04-10 16:43:00 +0700

    add base and config webpack to open feature: Module Federation

---
+++ projects/calendar/src/app/calendar/calendar-home/calendar-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './calendar-home.component.html',
+  styleUrl: './calendar-home.component.css'
+})
+export class CalendarHomeComponent {
+
+}
